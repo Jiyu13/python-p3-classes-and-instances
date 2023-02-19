7aaed43dc203ba0f1711e90463fbbe86973bfb00
Ziru Zou <jiyucoding@yahoo.com>
2023-02-19 17:29:05 -0500

    create a class and instances of that class, instantiate instances

---
+++ lib/main.py
@@ -0,0 +1,19 @@
+# defin a Class
+class Dog:
+    # some code to describe a dog
+    pass
+
+# new code goes here
+# create new instances/objects of class Dog
+fido = Dog()
+fido 
+# <__main__.Dog object at 0x7feb96d54fa0>
+
+
+snoopy = Dog()
+print(snoopy)
+# <__main__.Dog object at 0x7f6ba4e75a60>
+
+lassie = Dog()
+print(lassie)
+# <__main__.Dog object at 0x7f6ba4e5d790>
